b4d44181e94362b41c7989b1c550215b60be51da
Psychpsyo <psychpsyo@gmail.com>
2022-05-21 15:28:21 +0200

    Initial commit

---
+++ PrivacyPlus.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using System.Collections.Generic;
+using System;
+using System.Security.Cryptography;
+
+namespace PrivacyPlus
+{
+    public class PrivacyPlus : NeosMod {
+        public override string Name => "PrivacyPlus";
+        public override string Author => "Psychpsyo";
+        public override string Version => "1.0.0";
+        //public override string Link => "https://github.com/Psychpsyo/PrivacyPlus";
+
+        // config keys
+        [AutoRegisterConfigKey]
+        public static readonly ModConfigurationKey<string> UniqueDeviceIdentifierHash = new ModConfigurationKey<string>("Hashed UniqueDeviceIdentifier", "Sets your UniqueDeviceIdentifier Hash for sending to other users.", GenerateFakeUniqueDeviceIdentifierHash);
+
+        private static ModConfiguration config;
+        public override void OnEngineInit()
+        {
+            config = GetConfiguration();
+
+            Harmony harmony = new Harmony("Psychpsyo.PrivacyPlus");
+            harmony.PatchAll();
+        }
+
+        private static string GenerateFakeUniqueDeviceIdentifierHash()
+        {
+            var csprng = new RNGCryptoServiceProvider();
+            var bytes = new byte[32];
+
+            csprng.GetNonZeroBytes(bytes);
+            return String.Concat(Array.ConvertAll(bytes, x => x.ToString("X2")));
+        }
+
+
+        [HarmonyPatch(typeof(FrooxEngine.PlatformInterface), "GetExtraUserIds")]
+        class PrivacyPlusExIds
+        {
+            public static Dictionary<string, string> Postfix(Dictionary<string, string> originalExIds)
+            {
+                // return a new dict that only had our fake UniqueDeviceIdentifier hash
+                Dictionary<string, string> dict = new Dictionary<string, string>();
+                dict.Add("UniqueDeviceIdentifier", config.GetValue(UniqueDeviceIdentifierHash));
+                return new Dictionary<string, string>();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PrivacyPlus
+ Neos mod to hide your Discord & Steam info from other users.
